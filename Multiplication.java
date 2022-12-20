7b14c3ef5bf9d802e5d2eaa34f3bc234d8a1bbfe
YAGNAPRIYANKA <121090774+YAGNAPRIYANKA@users.noreply.github.com>
2022-12-20 14:40:38 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,18 @@
+improt java.io.*;
+class Multiplication
+{
+  public static void main(string args[])
+  {
+    int i,j,n;
+    Scanner S=new Scanner(System.in);
+    System.out.println("entr n value")
+    n=s.nextInt();
+    for(i=1;i<=m;i++)
+    {
+      for(j=1;j<n;j++)
+      {
+	System.out.println(i*j);
+      }
+    }
+  }
+}
